0ae54146924471f7a23d5a84b40500d0abe17535
dinhnhatphan251201 <dinhnhatphan251201@gmail.com>
2022-09-30 16:52:44 +0700

    RestFullAPi and build docker image springboot

---
+++ src/test/java/com/example/phandinhnhat_btth5/PhandinhnhatBtth5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phandinhnhat_btth5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhandinhnhatBtth5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
